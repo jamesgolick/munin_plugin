99997db23d581cc5c9d995e17777a649d11ee241
James Golick <james@giraffesoft.ca>
2010-01-04 16:20:22 -0800

    setup a simple api for defining munin plugins in a friendly way

---
+++ lib/munin_plugin.rb
@@ -0,0 +1,77 @@
+class MuninPlugin
+  class Attribute
+    attr_reader :name, :args
+
+    def initialize(*args)
+      if args.first.is_a?(Attribute)
+        @name = [args.shift.name, args.shift].join(".")
+      else
+        @name = args.first
+      end
+
+      @args = args
+    end
+
+    def method_missing(method, *args, &block)
+      @name = [name, method].join(".")
+      @args = args
+      self
+    end
+
+    def to_s
+      [name, *args].uniq.join(' ')
+    end
+  end
+
+  class Collector
+    undef_method :load
+
+    def initialize(&block)
+      instance_eval(&block)
+    end
+
+    def proxy_attributes
+      @proxy_attributes ||= []
+    end
+
+    def method_missing(method, *args, &block)
+      item = Attribute.new(method, *args, &block)
+      proxy_attributes << item
+      item
+    end
+
+    def collect(&block)
+      if block_given?
+        @collect = block
+      end
+      @collect
+    end
+
+    def to_s
+      proxy_attributes.map { |c| c.to_s }.join("\n") + "\n"
+    end
+  end
+
+  attr_reader :config
+  
+  def initialize(&block)
+    @config = Collector.new(&block)
+  end
+
+  def display_config
+    print config
+  end
+
+  def display_value
+    print Collector.new(&config.collect)
+  end
+
+  def run
+    ARGV.first == "config" ? display_config : display_value
+  end
+end
+
+# yeah, yeah :-)
+def munin_plugin(&block)
+  MuninPlugin.new(&block).run
+end
